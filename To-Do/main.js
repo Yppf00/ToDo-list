aaf81f414d20c708209777febc70e50e07bfe548
[ritual] ../*.shen <106227931+Yppf00@users.noreply.github.com>
2022-06-01 22:33:00 +0200

    Add files via upload

---
+++ To-Do/main.js
@@ -0,0 +1,15 @@
+let button = document.getElementById('button')
+button.addEventListener('click', function () {
+   let input = document.getElementById('input');
+   let container = document.getElementById('container');
+   let element = document.createElement('p')
+   element.innerText = input.value;
+   container.append(element);
+   input.value = '';
+   element.addEventListener('click', function () {
+      element.style.textDecoration = 'line-through'
+   })
+   element.addEventListener('dblclick', function () {
+      element.remove();
+   })
+});
